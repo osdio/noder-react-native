5cf2f017c2444e7b7aa038d24233cc8fa065edbe
soliury <lingyong1992@foxmail.com>
2016-02-03 12:19:29 +0800

    add strlen substring function

--- src/utils/index.js
+++ src/utils/index.js
@@ -39,3 +39,35 @@ export function link(url) {
 		}
 	});
 }
+
+export function substring(str, start, end) {
+	let result = '';
+	let i = 0;
+	while (i < end) {
+		let c = str.charCodeAt(i);
+		if ((c >= 0x0001 && c <= 0x007e) || (0xff60 <= c && c <= 0xff9f)) {
+			result += str[i];
+		}
+		else {
+			end -= 1;
+			result += str[i];
+		}
+		i++;
+	}
+	return result;
+}
+
+
+export function strlen(str) {
+	let len = 0;
+	for (let i = 0; i < str.length; i++) {
+		const c = str.charCodeAt(i);
+		if ((c >= 0x0001 && c <= 0x007e) || (0xff60 <= c && c <= 0xff9f)) {
+			len++;
+		}
+		else {
+			len += 2;
+		}
+	}
+	return len;
+}

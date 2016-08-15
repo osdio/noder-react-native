0bdd163c6c04c303435f597c9d97cce82f43adef
Li Zheng <flyskywhy@gmail.com>
2016-08-15 14:33:28 +0800

    Let JavaScript source map debugging not just locate to bundle.js but the real source file

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -23,7 +23,7 @@ var config = {
 module.exports = {
   ip: IP,
   port: PORT,
-  devtool: 'source-map',
+  devtool: 'cheap-module-eval-source-map',
   resolve: {
     alias: {
       'react-native': 'ReactWeb',
